1d0ce0ec83477e2e11110a542c55dc30e9e037d6
Timur Subasic <subasictimur@gmail.com>
2024-12-02 18:52:44 +0100

    contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,173 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+
+
+const formSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  phone: z.string().min(4, 'Enter valid number!').max(18, 'Enter valid number!'),
+  select: z.string(),
+  message: z.string().min(20, 'Please give us more information'),
+
+})
+
+
+
 
 export default function Contact() {
+
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      
+    },
+  })
+
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+
     return (
-      <div >
+      <div className=" max-w-6xl w-full mx-auto p-5 ">
         
-        contact
+        <section className="mb-10 mt-5">
+
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Name" {...field} />
+              </FormControl>
+              <FormDescription>
+                {/* Enter your name */}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="your@email.com" {...field} />
+              </FormControl>
+              <FormDescription>
+                {/* Enter your email */}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone Number</FormLabel>
+              <FormControl>
+                <Input placeholder="+123 45 678 910" {...field} />
+              </FormControl>
+              <FormDescription>
+                {/* Enter your phone number */}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="select"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Followers</FormLabel>
+              <FormControl>
+              <Select 
+              onValueChange={field.onChange}
+              value={field.value}>
+                <SelectTrigger className="">
+                  <SelectValue placeholder="Select your range" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="20000-50000">20,000-50,000</SelectItem>
+                  <SelectItem value="50000-250000">50,000-250,000</SelectItem>
+                  <SelectItem value="250000-50000">250,000-500,000</SelectItem>
+                  <SelectItem value="500000+">500,000+</SelectItem>
+                </SelectContent>
+              </Select>
+              </FormControl>
+              <FormDescription>
+                {/* Enter your followers */}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Tell us what you want to improve" {...field} />
+              </FormControl>
+              <FormDescription>
+                {/* Enter your message */}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <button className='uppercase font-bold px-5 py-3 rounded-lg bg-[#006039] text-white hover:opacity-85 hover:bg-yellow-500 hover:text-black hover:border-black border-4 border-[#006039] duration-300 w-full text-lg  ' type="submit">send</button>
+      </form>
+    </Form>
+
+        </section>
         
       </div>
     );
