f4acb0178179b43afe29287051344d584482864c
Timur Subasic <subasictimur@gmail.com>
2024-12-06 17:47:26 +0100

    .mov icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,7 +82,7 @@ export default function Home() {
             <div className='flex items-start justify-center gap-4'>
 
               <video 
-                src="/thunder.webm" 
+                src="/speed.mov" 
                 autoPlay 
                 loop 
                 muted 
@@ -108,7 +108,7 @@ export default function Home() {
             <div className='flex items-start justify-center gap-4'>
 
               <video 
-                src="/infinity.webm" 
+                src="/infinity.mov" 
                 autoPlay 
                 loop 
                 muted 
@@ -134,7 +134,7 @@ export default function Home() {
             <div className='flex items-start justify-center gap-4'>
 
               <video 
-                src="/hands.webm" 
+                src="/hands.mov" 
                 autoPlay 
                 loop 
                 muted 
